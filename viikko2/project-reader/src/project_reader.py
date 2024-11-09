32031ab884f35401df659d44cff7939300eb115c
Tony Kuosa <tony.kuosa@outlook.com>
2024-11-09 16:03:43 +0200

    vk2-t2: Riippuvuuksien hyödyntäminen. Toteutettu toiminnallisuus, joka lukee Project-olion tiedot .toml-muodossa, ja tulostaa niiden merkkijonoesityksen

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,5 +1,6 @@
 from urllib import request
 from project import Project
+import toml
 
 
 class ProjectReader:
@@ -9,7 +10,16 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
 
+        parsed_content = toml.loads(content)["tool"]["poetry"]
+
+        title = parsed_content["name"]
+        description = parsed_content["description"]
+        license = parsed_content["license"]
+
+        authors = [i for i in parsed_content["authors"]]
+        dependencies = [i for i in parsed_content["dependencies"]]
+        group_dev_dependencies = [i for i in parsed_content["group"]["dev"]["dependencies"]]
+        
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(title, description, license, authors, dependencies, group_dev_dependencies)
