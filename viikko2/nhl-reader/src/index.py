451dadb97e912770a55846189358d8570cfb1f42
Tony Kuosa <tony.kuosa@outlook.com>
2024-11-11 15:34:20 +0200

    vk2-t12: Koodin refaktorointia. Luotu uuteen tiedostoon playerutils.py luokat PlayerReader ja PlayerStats pelaajatietojen käsittelyyn ja palauttamiseen.

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,36 +1,20 @@
-import requests
-from player import Player
+from playerutils import PlayerReader, PlayerStats
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader.get_players())
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    players_by_country = {}
-    encountered_player_ids = set()
+    for player in players:
+        print(player)
+    
+    
+    all_players = stats.top_scorers_by_nationality()
 
-    for player_dict in response:
-        player = Player(player_dict)
-        country = player_dict['nationality']
-
-        if country not in players_by_country:
-            players_by_country[country] = set()
-
-        if player.id not in encountered_player_ids:
-            encountered_player_ids.add(player.id)
-            players_by_country[country].add(player)
-
-    print("Oliot:")
-
-    for country in players_by_country.keys():
-        print("Players from", country, "\n")
-        country_list = players_by_country[country]
-        for player in set(sorted(country_list, key=lambda player: (player.total, player.team), reverse=True)):
-            print("-", player)
-
-        print("")    
+    for player in all_players:
+        print(player)
 
 if __name__ == "__main__":
     main()
---
+++ viikko2/nhl-reader/src/playerutils.py
@@ -0,0 +1,50 @@
+from player import Player
+import requests
+
+class PlayerReader:
+    def __init__(self, url):
+        self.response = requests.get(url).json()
+    
+    def get_players(self):
+        players_by_country = {}
+        encountered_player_ids = set()
+
+        for player_dict in self.response:
+            player = Player(player_dict)
+            country = player_dict['nationality']
+
+            if country not in players_by_country:
+                players_by_country[country] = set()
+
+            if player.id not in encountered_player_ids:
+                encountered_player_ids.add(player.id)
+                players_by_country[country].add(player)
+
+        return players_by_country
+
+class PlayerStats:
+    def __init__(self, players_dict):
+        self.players_dict = players_dict
+
+    def top_scorers_by_nationality(self, country=None):
+        # If a country is not given, this function returns all players grouped by nationality
+
+        result = []
+
+        if country is not None:
+            country_players = self.players_dict[country]
+            result.append(f"Players from {country} \n")
+
+            for player in sorted(country_players, key=lambda player: player.total, reverse=True):
+                result.append(player)
+        
+        else:
+            for country in self.players_dict.keys():
+                result.append(f"Players from {country} \n")
+                country_players = self.players_dict[country]
+
+                for player in sorted(country_players, key=lambda player: player.total, reverse=True):
+                    result.append(player)
+
+        result.append("")
+        return result
