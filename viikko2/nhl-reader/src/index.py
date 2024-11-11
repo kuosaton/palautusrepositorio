c8b72f1209683eeb9247e208484071f4d7ff0563
Tony Kuosa <tony.kuosa@outlook.com>
2024-11-11 14:03:20 +0200

    vk2-t10+t11: luotu toiminnallisuus, joka palauttaa merkkijonoesityksenä NHL-pelaajien JSON-muotoisen aineiston pelaajat maittain ryhmiteltynä total points -järjestyksessä

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,31 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    response = requests.get(url).json()
+
+    print("JSON-muotoinen vastaus:")
+    print(response)
+    players_by_country = {}
+
+    for player_dict in response:
+        player = Player(player_dict)
+        country = player_dict['nationality']
+
+        if country not in players_by_country:
+            players_by_country[country] = []
+
+        players_by_country[country].append(player)
+
+    print("Oliot:")
+
+    for country in players_by_country.keys():
+        print("Players from", country, "\n")
+        country_list = players_by_country[country]
+        for player in sorted(country_list, key=lambda player: player.total, reverse=True):
+            print(player)
+        print("")
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,11 @@
 class Player:
     def __init__(self, dict):
+        self.nationality = dict['nationality']
         self.name = dict['name']
-    
+        self.team = dict['team']
+        self.goals = dict['goals']
+        self.assists = dict['assists']
+        self.total = (int(self.goals + self.assists))
+
     def __str__(self):
-        return self.name
+        return f"{self.name:20} {self.team} {self.goals} + {self.assists} = {self.total}"
