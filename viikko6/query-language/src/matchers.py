304e92ce77908fb8d24ce134dbe91bc5c647e0ac
Tony Kuosa <tony.kuosa@outlook.com>
2024-12-05 15:05:04 +0200

    vk6-tk2

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -27,3 +27,38 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+
+class All:
+    def __init__(self, *players):
+        self._players = players
+
+    def test(self, *matchers):
+        for player in self._players:
+            for matcher in matchers:
+                if not matcher.test(player):
+                    return False
+        return True
+
+
+class Not:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return False
+
+        return True
+
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
