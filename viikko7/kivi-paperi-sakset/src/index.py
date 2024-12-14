3a14d3865fe8c72f613bb7f85b58c1644e867e01
Tony Kuosa <tony.kuosa@outlook.com>
2024-12-14 14:56:22 +0200

    vk7-t3+4: toteuta pelaa-metodi template-metodina uuteen kps.py -yleistiedostoon. peliolion luominen toteutetaan peliolio.py -yleistiedostossa.

--- viikko7/kivi-paperi-sakset/src/index.py
+++ viikko7/kivi-paperi-sakset/src/index.py
@@ -1,16 +1,15 @@
-from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
-from kps_tekoaly import KPSTekoaly
-from kps_parempi_tekoaly import KPSParempiTekoaly
+from peliolio import Kaksinpeli, Yksinpeli, HaastavaYksinpeli
 
 
 def main():
     while True:
-        print("Valitse pelataanko"
-              "\n (a) Ihmistä vastaan"
-              "\n (b) Tekoälyä vastaan"
-              "\n (c) Parannettua tekoälyä vastaan"
-              "\nMuilla valinnoilla lopetetaan"
-              )
+        print(
+            "Valitse pelataanko"
+            "\n (a) Ihmistä vastaan"
+            "\n (b) Tekoälyä vastaan"
+            "\n (c) Parannettua tekoälyä vastaan"
+            "\nMuilla valinnoilla lopetetaan"
+        )
 
         vastaus = input()
 
@@ -19,21 +18,23 @@ def main():
                 "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
             )
 
-            kaksinpeli = KPSPelaajaVsPelaaja()
+            kaksinpeli = Kaksinpeli()
             kaksinpeli.pelaa()
+
         elif vastaus.endswith("b"):
             print(
                 "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
             )
 
-            yksinpeli = KPSTekoaly()
+            yksinpeli = Yksinpeli()
             yksinpeli.pelaa()
+
         elif vastaus.endswith("c"):
             print(
                 "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
             )
 
-            haastava_yksinpeli = KPSParempiTekoaly()
+            haastava_yksinpeli = HaastavaYksinpeli()
             haastava_yksinpeli.pelaa()
         else:
             break
---
+++ viikko7/kivi-paperi-sakset/src/peliolio.py
@@ -0,0 +1,27 @@
+from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
+from kps_tekoaly import KPSTekoaly
+from kps_parempi_tekoaly import KPSParempiTekoaly
+
+
+class Kaksinpeli:
+    def __init__(self):
+        self._peli = KPSPelaajaVsPelaaja()
+
+    def pelaa(self):
+        self._peli.pelaa()
+
+
+class Yksinpeli:
+    def __init__(self):
+        self._peli = KPSTekoaly()
+
+    def pelaa(self):
+        self._peli.pelaa()
+
+
+class HaastavaYksinpeli:
+    def __init__(self):
+        self._peli = KPSParempiTekoaly
+
+    def pelaa(self):
+        self._peli.pelaa()
